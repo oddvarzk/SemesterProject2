7948ff1d4e657f345ae03b2faec4f02b69a96012
Oddvar Z Kristiansen <122876488+oddvarzk@users.noreply.github.com>
2024-05-13 17:19:20 +0200

    navbar folder seperated for better file sorting

--- js/navbar/index.js
+++ js/navbar/index.js
@@ -1,15 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Select all dropdown toggle buttons
     const dropdownToggles = document.querySelectorAll(".dropdown-toggle")
   
     dropdownToggles.forEach((toggle) => {
       toggle.addEventListener("click", () => {
-        // Find the next sibling element which is the dropdown menu
         const dropdownMenu = toggle.nextElementSibling
   
-        // Toggle the 'hidden' class to show or hide the dropdown menu
         if (dropdownMenu.classList.contains("hidden")) {
-          // Hide any open dropdown menus before showing the new one
           document.querySelectorAll(".dropdown-menu").forEach((menu) => {
             menu.classList.add("hidden")
           })
@@ -21,7 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
       })
     })
   
-    // Clicking outside of an open dropdown menu closes it
     window.addEventListener("click", function (e) {
       if (!e.target.matches(".dropdown-toggle")) {
         document.querySelectorAll(".dropdown-menu").forEach((menu) => {
@@ -32,15 +27,11 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     })
     
-    // Mobile menu toggle
-    
     const mobileMenuButton = document.querySelector('.mobile-menu-button')
     const mobileMenu = document.querySelector('.navigation-menu')
     
     mobileMenuButton.addEventListener('click', () => {
       mobileMenu.classList.toggle('hidden')
     })
-    
-    
   })
   
