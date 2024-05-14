5b1d6aaa55ae1431ed0e3203e5edf99c684249ba
Oddvar Kristiansen <oddvarzk@gmail.com>
2024-05-14 18:48:16 +0200

    tailwind config altered to check a select few files and not go threw entire project for better reload time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
-const module = ('module');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html',
-    './auctionPage/index.html',
-    './biddingPage/index.html',
-    './loginPage/index.html',
-    './profilePage/index.html',
-    './registerPage/index.html'
+    "./styles/output.css", // Output from 'npm run build:css'
+    "./index.html",
+    "./auctionPage/index.html",
+    "./biddingPage/index.html",
+    "./loginPage/index.html",
+    "./profilePage/index.html",
+    "./registerPage/index.html",
   ],
   theme: {
     extend: {
-      fontFamily:{
-        'playfair': ["Playfair+Display", "serif"],
-        'serifFour': ["Source+Serif+4", "serif"],
-      }
+      fontFamily: {
+        playfair: ["Playfair+Display", "serif"],
+        serifFour: ["Source+Serif+4", "serif"],
+      },
     },
   },
   plugins: [],
